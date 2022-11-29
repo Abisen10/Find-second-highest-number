5f40467a3b8192e0865874ce7f7a692a3033c354
ANKUR JAGDISH BISEN <87488680+Abisen10@users.noreply.github.com>
2022-11-29 06:46:51 -0800

    Add files via upload

---
+++ SEC LAST NUMBER .PY.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def second_highest(list):
+    list.sort()
+    return list[-2]
+
+list = [1,2,3,4,5,6]
+
+print("Second highest number from list is: ", second_highest(list))
+
+
+# In[3]:
+
+
+def second_highest(list):
+    list.sort()
+    return list[-2]
+
+list = [1,2,3,4,5,6,7,8,9]
+
+print("Second highest number from list is: ", second_highest(list))
+
+
+# In[ ]:
+
+
+
+
